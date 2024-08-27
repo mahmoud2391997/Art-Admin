573b85f0e224b77fdd260d241878685e8d44940b
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2024-08-27 22:30:04 +0300

    Statistics card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import CardStats from '../components/CardStats/CardStats'
 
 export default function Home() {
+  const arr = [{
+    statSubtitle: "Total Income",
+    statTitle: "350,897",
+    statArrow: "down",
+    statPercent: "3.48",
+    statDescripiron: "Since last month",
+    statIconName: "Income",
+  },{
+    statSubtitle: "Total Profit",
+    statTitle: "350,897",
+    statArrow: "down",
+    statPercent: "3.48",
+    statDescripiron: "Since last month",
+    statIconName: "Profit",
+  },{
+    statSubtitle: "Total Sales",
+    statTitle: "350,897",
+    statArrow: "up",
+    statPercent: "3.48",
+    statDescripiron: "Since last month",
+    statIconName: "Sales",
+  },{
+    statSubtitle: "Total Visitors",
+    statTitle: "350,897",
+    statArrow: "up",
+    statPercent: "12.3",
+    statDescripiron: "Since last month",
+    statIconName: "View",
+  }]
   return (
-    <div>
-      Home Page
+    <div className='grid gap-5 w-[75%] lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 p-5'>
+    {arr.map((card)=>{
+      return <CardStats statPercent={card.statPercent} statSubtitle={card.statSubtitle} statIconName={card.statIconName} statArrow={card.statArrow}/>
+    })}
     </div>
   )
 }

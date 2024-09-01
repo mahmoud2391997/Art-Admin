a70347fc2b0ad5dc7f6822dbe615b0f99626a279
BaselMohab <baselmohab99@gmail.com>
2024-09-01 17:44:35 +0300

    handled Product Form UI, added Products title background

--- src/components/Products/ProductForm.jsx
+++ src/components/Products/ProductForm.jsx
@@ -1,81 +1,83 @@
-// ProductForm Component
-import { Button, Input, Dialog, DialogHeader, DialogBody, DialogFooter } from '@material-tailwind/react';
+import { Button, Input, Dialog, DialogBody, DialogFooter, DialogHeader } from '@material-tailwind/react';  
 
-export default function ProductForm({ open, isEditing, newProduct, setNewProduct, onSave, onClose }) {
-    return (
-        <Dialog open={open} handler={onClose} className="max-w-md mx-auto">
-            <DialogHeader>{isEditing ? 'Edit Product' : 'Add Product'}</DialogHeader>
-            <DialogBody>
-                <div className="space-y-4">
-                    {isEditing && (
-                        <Input
-                            type="text"
-                            label="Product ID"
-                            value={newProduct._id}
-                            onChange={(e) => setNewProduct({ ...newProduct, _id: e.target.value })}
-                            className="my-2"
-                            disabled
-                        />
-                    )}
-                    <Input
-                        type="text"
-                        label="Product Name"
-                        value={newProduct.name}
-                        onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
-                        className="my-2"
-                    />
-                    <Input
-                        type="number"
-                        label="Product Price"
-                        value={newProduct.price}
-                        onChange={(e) => setNewProduct({ ...newProduct, price: parseFloat(e.target.value) })}
-                        className="my-2"
-                    />
-                    <Input
-                        type="text"
-                        label="Product Description"
-                        value={newProduct.description}
-                        onChange={(e) => setNewProduct({ ...newProduct, description: e.target.value })}
-                        className="my-2"
-                    />
-                    <Input
-                        type="number"
-                        label="Stock"
-                        value={newProduct.stock}
-                        onChange={(e) => setNewProduct({ ...newProduct, stock: parseInt(e.target.value, 10) })}
-                        className="my-2"
-                    />
-                    <Input
-                        type="text"
-                        label="Product Image URL"
-                        value={newProduct.image}
-                        onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}
-                        className="my-2"
-                    />
-                    <Input
-                        type="text"
-                        label="Status"
-                        value={newProduct.status}
-                        onChange={(e) => setNewProduct({ ...newProduct, status: e.target.value })}
-                        className="my-2"
-                    />
-                    <Input
-                        type="text"
-                        label="Category Name"
-                        value={newProduct.categoryName}
-                        onChange={(e) => setNewProduct({ ...newProduct, categoryName: e.target.value })}
-                        className="my-2"
-                    />
-                </div>
-            </DialogBody>
-            <DialogFooter>
-                <Button color="red" onClick={onClose} className="mr-2">
-                    Cancel
-                </Button>
-                <Button color="blue" onClick={onSave}>
-                    {isEditing ? 'Update Product' : 'Add Product'}
-                </Button>
-            </DialogFooter>
-        </Dialog>
-    );
-}
+export default function ProductForm({ open, isEditing, newProduct, setNewProduct, onSave, onClose }) {  
+    return (  
+        <Dialog open={open} handler={onClose} className="relative z-40 bg-white max-w-xl mx-auto shadow-xl">  
+            <DialogBody>  
+                <div className="p-8 font-eb-garamond text-gray-700">  
+                    <DialogHeader className="text-2xl font-semibold mb-6 uppercase">{isEditing ? 'Edit Product' : 'Add Product'}</DialogHeader>  
+                    <div className="grid grid-cols-1 gap-8 ">  
+                        {/* Input Fields */}  
+                        {isEditing && (  
+                            <Input  
+                                type="text"  
+                                placeholder="Product ID"  
+                                value={newProduct._id}  
+                                onChange={(e) => setNewProduct({ ...newProduct, _id: e.target.value })}  
+                                className="w-full border-b border-gray-400 p-2 placeholder-gray-500"  
+                                disabled  
+                            />  
+                        )}  
+                        <Input  
+                            type="text"  
+                            placeholder="Product Name"  
+                            value={newProduct.name}  
+                            onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}  
+                            className="w-full border-b border-gray-400 p-2 placeholder-gray-500"  
+                        />  
+                        <Input  
+                            type="number"  
+                            placeholder="Product Price"  
+                            value={newProduct.price}  
+                            onChange={(e) => setNewProduct({ ...newProduct, price: parseFloat(e.target.value) })}  
+                            className="w-full border-b border-gray-400 p-2 placeholder-gray-500"  
+                        />  
+                        <Input  
+                            type="text"  
+                            placeholder="Product Description"  
+                            value={newProduct.description}  
+                            onChange={(e) => setNewProduct({ ...newProduct, description: e.target.value })}  
+                            className="w-full border-b border-gray-400 p-2 placeholder-gray-500"  
+                        />  
+                        <Input  
+                            type="number"  
+                            placeholder="Stock"  
+                            value={newProduct.stock}  
+                            onChange={(e) => setNewProduct({ ...newProduct, stock: parseInt(e.target.value, 10) })}  
+                            className="w-full border-b border-gray-400 p-2 placeholder-gray-500"  
+                        />  
+                        <Input  
+                            type="text"  
+                            placeholder="Product Image URL"  
+                            value={newProduct.image}  
+                            onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}  
+                            className="w-full border-b border-gray-400 p-2 placeholder-gray-500"  
+                        />  
+                        <Input  
+                            type="text"  
+                            placeholder="Status"  
+                            value={newProduct.status}  
+                            onChange={(e) => setNewProduct({ ...newProduct, status: e.target.value })}  
+                            className="w-full border-b border-gray-400 p-2 placeholder-gray-500"  
+                        />  
+                        <Input  
+                            type="text"  
+                            placeholder="Category Name"  
+                            value={newProduct.categoryName}  
+                            onChange={(e) => setNewProduct({ ...newProduct, categoryName: e.target.value })}  
+                            className="w-full border-b border-gray-400 p-2 placeholder-gray-500"  
+                        />  
+                    </div>  
+                </div>  
+            </DialogBody>  
+            <DialogFooter className='ml-8 mb-2'>  
+                <Button onClick={onClose} className="mr-8 text-main">  
+                    Cancel  
+                </Button>  
+                <Button onClick={onSave} className='text-main'>  
+                    {isEditing ? 'Update Product' : 'Add Product'}  
+                </Button>  
+            </DialogFooter>  
+        </Dialog>  
+    );  
+}  
